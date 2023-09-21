97408c83ee1a2a207380a37bc4ce2245276e397e
cvaisnor <chrisvaisnor@gmail.com>
2023-09-20 23:22:29 -0700

    added 3 retries if can't find object

--- src/tracking_functions.py
+++ src/tracking_functions.py
@@ -97,6 +97,8 @@ def track_object_steppers(camera_capture,
 
     first_search = True
 
+    search_retry_count = 0
+
     while True:
         # read new frame after cropping the object
         frame = capture_single_frame(camera_capture)
@@ -107,7 +109,7 @@ def track_object_steppers(camera_capture,
             first_search = False
         else:
             # search for the template match using a less expensive method
-            max_val, max_loc = check_image_match_local(frame, cropped_object_image, max_loc)
+            max_val, max_loc = check_image_match_local(frame, cropped_object_image, last_loc=max_loc, obj_padding=200)
 
         # condition to check object is matched or not
         if max_val > template_matching_threshold:
@@ -120,8 +122,14 @@ def track_object_steppers(camera_capture,
 
             # take the average of all object images in the list
             cropped_object_image = np.average(all_cropped_objects, axis=0).astype(np.uint8)
-        else:
+        elif max_val < template_matching_threshold - 0.1:
+            print('Object not found, retrying')
+            first_search = True # change to True to search the entire frame
+            search_retry_count += 1 # increment the search retry count
+            continue
+        elif search_retry_count > 3: # if the object is not found after 3 retries, stop searching
             # video_writer.release()
+            print('Object not found after 3 retries')
             cv2.destroyAllWindows()
             return False
             
@@ -133,7 +141,7 @@ def track_object_steppers(camera_capture,
         cv2.imshow("Now tracking", frame_copy)
 
         # display the new cropped object image (debugging)
-        # cv2.imshow("Running Average", cv2.resize(cropped_object_image, (640, 480)))
+        cv2.imshow("Running Average", cv2.resize(cropped_object_image, (640, 480)))
 
         # calculate the difference between the center of the frame and the center of the match
         difference_x = (frame.shape[1] / 2) - (max_loc[0] + cropped_object_image.shape[1] / 2)
