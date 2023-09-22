f54282471e6a5d729dcff324a6e8cf5acfffb5d4
cvaisnor <chrisvaisnor@gmail.com>
2023-09-21 23:06:40 -0700

    gopro working as video cap device

---
+++ tests/test_gopro.py
@@ -0,0 +1,34 @@
+import cv2
+
+from gopro_webcam import GoProWebcamPlayer
+
+
+# https://github.com/gopro/OpenGoPro/blob/main/demos/python/multi_webcam/multi_webcam/webcam.py
+gopro = GoProWebcamPlayer(serial='646')
+
+gopro.open()
+
+# https://gopro.github.io/OpenGoPro/python_sdk/api.html#open_gopro.api.params.WebcamFOV
+
+gopro.webcam.start(gopro.port, resolution=12, fov=0)
+
+gopro.player.url = GoProWebcamPlayer.STREAM_URL.format(port=gopro.port)
+
+video_capture = cv2.VideoCapture(gopro.player.url + "?overrun_nonfatal=1&fifo_size=50000000", cv2.CAP_FFMPEG)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    # Display the resulting frame
+    cv2.imshow('GoPro Webcam', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture
+video_capture.release()
+
+gopro.close()
+
+cv2.destroyAllWindows()
