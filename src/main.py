5485a08acfafa856a023f5b13eca754cead5036b
cvaisnor <chrisvaisnor@gmail.com>
2023-09-27 02:13:06 -0700

    refactoring out contour logic

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@
 import time
 import cv2
 
-from camera_functions import get_cropped_object_image, get_contours, get_largest_contour
+from camera_functions import get_cropped_object_image, contour_parser
 from tracking_functions import track_object
 
 
@@ -30,8 +30,11 @@ def main():
     print('-'*30)
     
     ret, background_frame = camera_capture.read()
-
-    time.sleep(2)
+    if ret is False:
+        print('Error reading first background frame')
+        return
+    
+    cv2.imshow("Background View", background_frame) # display the frame
 
     CONTOUR_THRESHOLD_VALUE = 40.0 # pixel value threshold for contour detection
     MIN_AREA = 5 # minimum area of the contour
@@ -43,50 +46,29 @@ def main():
 
     number_of_objects = 0 # number of objects detected
     while True:
-
-        captured_object = False
-
         # capture another frame
         ret, current_frame = camera_capture.read()
         if ret is False:
             print('Error reading frame')
             continue
 
-        contours, threshold_frame, difference_frame = get_contours(background_frame, current_frame, threshold_value=CONTOUR_THRESHOLD_VALUE)
+        contour_found, x, y, w, h = contour_parser(current_frame,
+                                                    background_frame,
+                                                    CONTOUR_THRESHOLD_VALUE,
+                                                    MIN_AREA,
+                                                    MAX_AREA,
+                                                    frame_width=WIDTH,
+                                                    frame_height=HEIGHT)
 
-        current_frame_copy = current_frame.copy()
-
-        if len(contours) > 0:
-            largest_countour = get_largest_contour(contours, min_area=MIN_AREA, max_area=MAX_AREA) # find the object with the largest contour
-            
-            if largest_countour is None:
-                print('No contours found that meet the area requirements.')
-                continue
-
-            # get the x, y, width, and height of box around the contour
-            x_of_contour, y_of_contour, width_of_contour, height_of_contour = cv2.boundingRect(largest_countour)
-
-            # draw a box around the contour
-            cv2.rectangle(current_frame_copy, (x_of_contour, y_of_contour), (x_of_contour + width_of_contour, y_of_contour + height_of_contour), (0, 255, 0), 2)
-
-            if x_of_contour + width_of_contour > WIDTH or y_of_contour + height_of_contour > HEIGHT: # if the contour is too close to the edge of the frame, ignore it
-                print('Contour too close to edge of frame')
-                continue
-            captured_object = True
+        if contour_found:
             number_of_objects += 1
-
-        # display the frames
-        cv2.imshow("Threshold Frame", threshold_frame)
-        cv2.imshow("Background View", current_frame_copy)
-
-        if captured_object:
             # add pixels to each side of the contour to get a buffer
-            x_of_contour -= OBJECT_BUFFER
-            y_of_contour -= OBJECT_BUFFER
-            width_of_contour += 2*OBJECT_BUFFER
-            height_of_contour += 2*OBJECT_BUFFER
+            x -= OBJECT_BUFFER
+            y -= OBJECT_BUFFER
+            w += 2*OBJECT_BUFFER
+            h += 2*OBJECT_BUFFER
 
-            cropped_object_image = get_cropped_object_image(current_frame, x_of_contour, y_of_contour, width_of_contour, height_of_contour) # crop the object from the current frame
+            cropped_object_image = get_cropped_object_image(current_frame, x, y, w, h) # crop the object from the current frame
 
             # filename to save the captured object image
             filename = f'captured_objects/captured_object_{number_of_objects}.jpg'
@@ -95,7 +77,7 @@ def main():
             cv2.imwrite(filename, cropped_object_image)
 
             # display the cropped object image at a larger size (debugging)
-            cv2.imshow("Tracking Object (original)", cv2.resize(cropped_object_image, (640, 480)))
+            # cv2.imshow("Tracking Object (original)", cv2.resize(cropped_object_image, (640, 480)))
 
             print('Tracking object')
 
@@ -106,21 +88,24 @@ def main():
                                 frames_to_average=FRAMES_TO_AVERAGE,
                                 number_of_objects=number_of_objects,
                                 gimbal_movement=GIMBAL_MOVEMENT)
-                
+
             if switch_to_motion_detection:
                 print('Finished tracking object')
                 print()
+
+                # flush the frames in the buffer
+                for i in range(60):
+                    camera_capture.grab()
 
                 # capture a new background frame
                 ret, background_frame = camera_capture.read()
                 continue
 
-        if (captured_object and not switch_to_motion_detection) or cv2.waitKey(1) & 0xFF == ord('q'):
+        if (contour_found and not switch_to_motion_detection) or cv2.waitKey(1) & 0xFF == ord('q'):
             # cleanup
             camera_capture.release()
             cv2.destroyAllWindows()
             print('Closing Program')
-            # gopro.close()
             break
 
 
