17d18d42afdd493015885f49760d500eb9d999b4
cvaisnor <chrisvaisnor@gmail.com>
2023-10-03 23:32:43 -0700

    moved imports

--- src/stepper_gimbal_functions.py
+++ src/stepper_gimbal_functions.py
@@ -1,4 +1,7 @@
 import time
+import sys
+import tty
+import termios
 
 from classes import SerialConnection, Message, MessageCommand, MotorState, MotorDirection, MotorSpeed
 
@@ -22,9 +25,6 @@ def move_steppers(connection: SerialConnection, pan: MotorState, tilt: MotorStat
 
 def read_keypress() -> int:
     '''Read keypress from the user.'''
-    import sys
-    import tty
-    import termios
 
     fd = sys.stdin.fileno()
     old_settings = termios.tcgetattr(fd)
@@ -41,29 +41,29 @@ def read_keypress() -> int:
 def move_gimbal_with_keypress(connection, pan_state, tilt_state):
     '''Manual control of the gimbal using the keyboard.'''
 
-    keypress = read_keypress()
-    if keypress == 255:
-        return keypress
+    raw_keypress = read_keypress()
+    if raw_keypress == 255:
+        return raw_keypress
 
-    if keypress == ord('w'):
+    if raw_keypress == ord('w'):
         # print('Moving up')
+        pan_state.speed = MotorSpeed.Off
+        pan_state.direction = MotorDirection.Zero
         tilt_state.speed = MotorSpeed.Speed4
         tilt_state.direction = MotorDirection.Up
+    elif raw_keypress == ord('s'):
+        # print('Moving down')
         pan_state.speed = MotorSpeed.Off
         pan_state.direction = MotorDirection.Zero
-    elif keypress == ord('s'):
-        # print('Moving down')
         tilt_state.speed = MotorSpeed.Speed4
         tilt_state.direction = MotorDirection.Down
-        pan_state.speed = MotorSpeed.Off
-        pan_state.direction = MotorDirection.Zero
-    elif keypress == ord('a'):
+    elif raw_keypress == ord('a'):
         # print('Moving left')
         pan_state.speed = MotorSpeed.Speed4
         pan_state.direction = MotorDirection.Zero
         tilt_state.speed = MotorSpeed.Off
         tilt_state.direction = MotorDirection.Zero
-    elif keypress == ord('d'):
+    elif raw_keypress == ord('d'):
         # print('Moving right')
         pan_state.speed = MotorSpeed.Speed4
         pan_state.direction = MotorDirection.One
@@ -77,7 +77,7 @@ def move_gimbal_with_keypress(connection, pan_state, tilt_state):
         tilt_state.direction = MotorDirection.Zero
 
     move_steppers(connection, pan_state, tilt_state)
-    return keypress
+    return raw_keypress
 
 
 if __name__ == '__main__':
