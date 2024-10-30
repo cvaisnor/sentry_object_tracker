cfc585bef29c8e9d2ec2c5c40f57f5b7c3af7b7c
cvaisnor <chrisvaisnor@gmail.com>
2024-10-30 13:55:50 -0700

    new endstop test

---
+++ stepper_serial_control/endstop_test.ino
@@ -0,0 +1,21 @@
+// Define endstop pin
+#define PAN_LIMIT_PIN  9
+#define TILT_LIMIT_PIN 10
+
+void setup() {
+  Serial.begin(115200);
+  
+  // Configure endstop pins
+  pinMode(PAN_LIMIT_PIN, INPUT_PULLUP);
+  pinMode(TILT_LIMIT_PIN, INPUT_PULLUP);
+}
+
+void loop() {
+  // small if statement to check if an endstop is triggered
+    if (digitalRead(PAN_LIMIT_PIN) == HIGH) {
+        Serial.println("Pan Endstop triggered!");
+    }
+    if (digitalRead(TILT_LIMIT_PIN) == HIGH) {
+        Serial.println("Tilt Endstop triggered!");
+    }
+}
